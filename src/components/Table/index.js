7ad9004cc7ce0a1198b30b69fac557c1f8a7bc75
bfall-debug <bfall@invengeon.com>
2020-02-18 23:29:00 -0500

    Component tree fully built

---
+++ src/components/Table/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TableHeader from "./TableHeader";
+import TableRow from "./TableRow";
+
+export default function Table()  {
+    return (
+        <div>
+            <TableHeader/>
+            <TableRow></TableRow>
+        </div>
+    );
+}
