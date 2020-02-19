2bf332a01c565c7f4275bd56607261b1b97a5116
bfall-debug <bfall@invengeon.com>
2020-02-19 01:37:28 -0500

    Non-essential components removed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.css';
-import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
-import Home from "./pages/Home";
+import Table from "./components/Table";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <Wrapper>
-        <Home/>
-      </Wrapper>
+      <Table />
     </div>
   );
 }
